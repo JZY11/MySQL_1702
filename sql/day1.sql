f4c4a423857e4fed7d1da13dabd9a05490eaa7b0
Tony.Jaa <1291813139@qq.com>
2017-05-15 17:43:59 +0800

    update

--- sql/day1.sql
+++ sql/day1.sql
@@ -1,1 +1,18 @@
 show DATABASES ;
+CREATE DATABASE db_1712;
+DROP DATABASE db_1712;
+USE db_1712;
+SHOW TABLES ;
+CREATE TABLE t_student (
+  sno   VARCHAR(10),
+  sname VARCHAR(10),
+  gender CHAR(1),
+  age INT(2)
+);
+SELECT *
+FROM t_student;
+
+INSERT INTO t_student
+    VALUE ('2017001','张三','男',18)
+
+
