522a54a09897d015395b99f9202946b0ee978d5a
Tony.Jaa <1291813139@qq.com>
2017-05-16 10:40:25 +0800

    update

--- sql/day2.sql
+++ sql/day2.sql
@@ -1,13 +1,19 @@
 SHOW DATABASES ;
 
 DROP TABLE db_1712.student;
-CREATE TABLE db_1712.student(
-  name VARCHAR(255),
-  intro TEXT,
-  gender CHAR(2)
+CREATE TABLE db_1712.student (
+  name   VARCHAR(255),
+  intro  TEXT,
+  gender CHAR(2),
+  age int,
+  height DOUBLE(3,2),
+  price DECIMAL(6,2),
+  dob DATE,    -- data of birth
+  time DATETIME
 );
+
 INSERT INTO db_1712.student VALUES (
-    'Tom','sdahddbha','M'
+    'Tom','sdahddbha','M',18.0,1.7,123.46,'1999-5-1','2017-5-16-10-39-01'
 );
 SELECT *
 FROM db_1712.student;
