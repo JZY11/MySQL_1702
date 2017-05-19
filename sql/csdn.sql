6eec916335d478783c70170a49fce5f2cc99148c
Tony.Jaa <1291813139@qq.com>
2017-05-20 00:43:30 +0800

    updata

---
+++ sql/csdn.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS db_1712.csdn;
+CREATE TABLE db_1712.csdn (
+  id       INT AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'ID,PK',
+  username VARCHAR(255) COMMENT '用户名',
+  password VARCHAR(255) COMMENT '密码',
+  email    VARCHAR(255) COMMENT '邮箱'
+)
+  COMMENT 'csdn用户表';
+
+SELECT *
+FROM db_1712.csdn;
+
+LOAD DATA LOCAL INFILE '/Users/Tony.Jaa/Desktop/www.csdn.net.sql'
+    INTO TABLE db_1712.csdn
+FIELDS TERMINATED BY ' # '(username,password,email  )
+SET id = NULL ;
