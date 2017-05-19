b224d396f3ccab8d7c2577022239e11d9ca09963
Tony.Jaa <1291813139@qq.com>
2017-05-19 10:53:25 +0800

    updata

--- sql/day7.sql
+++ sql/day7.sql
@@ -32,4 +32,42 @@ FROM scott.emp;
 
 SELECT *
 FROM scott.emp
-WHERE ENAME RLIKE '[0-9]+';
+WHERE ENAME RLIKE '[a-f]+';-- 正则匹配的关键字可以为RLIKE 或 REGEXP    ^取反 [^a-f]表示匹配不包含a-f之外的任一字母
+
+SELECT *
+FROM scott.emp
+WHERE HIREDATE REGEXP '-02$';-- -02- 匹配2月份  $结尾
+
+SELECT *
+FROM scott.emp
+WHERE DEPTNO = 10 OR DEPTNO = 20;
+
+SELECT *
+FROM scott.emp
+WHERE DEPTNO NOT IN (10,20); -- IN  排除10与20的部门
+
+SELECT *
+FROM scott.emp
+WHERE SAL NOT BETWEEN 2000 AND 3000; -- [min, max]    BETWEEN  AND
+
+SELECT *
+FROM scott.emp
+WHERE JOB IN ('manager', 'clerk');
+
+SELECT *
+FROM scott.emp
+WHERE SAL >= 2000 AND SAL<= 3000;
+
+SELECT ENAME '姓名',SAL '基本工资'  -- 别名alias  给字段起别名
+FROM scott.emp;
+
+SELECT *
+FROM scott.emp AS e ;-- 为表起别名
+
+SELECT e.ENAME
+FROM scott.emp 'e';
+
+SELECT *
+FROM scott.emp
+WHERE DEPTNO IS NOT NULL ; -- 检索null
+
