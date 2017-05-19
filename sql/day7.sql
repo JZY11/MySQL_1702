5da18d523fe76151acc77de5c073363bfed5188f
Tony.Jaa <1291813139@qq.com>
2017-05-19 09:53:28 +0800

    updata

--- sql/day7.sql
+++ sql/day7.sql
@@ -17,3 +17,19 @@ SELECT *
 FROM scott.emp
 ORDER BY ENAME
 LIMIT 3 OFFSET 0;-- limit 位于order by 之后
+
+SELECT *       -- 通配符%   _
+FROM scott.emp
+WHERE ENAME LIKE 'a%';-- like像   模糊匹配   'a%'匹配以字母a开头   ‘%a%’含有a   %匹配任意长度
+
+SELECT *
+FROM scott.emp
+WHERE ENAME LIKE '_DAMS';-- _匹配一个字符，字符任意   与%有区别
+
+INSERT INTO scott.emp (EMPNO,ENAME) VALUE (222,'A_ADAMS');
+SELECT *
+FROM scott.emp;
+
+SELECT *
+FROM scott.emp
+WHERE ENAME RLIKE '[0-9]+';
