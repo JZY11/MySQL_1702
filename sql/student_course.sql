409271ef09cad61b711ca69f6005024da586109e
Tony.Jaa <1291813139@qq.com>
2017-05-23 19:13:12 +0800

    updata

--- sql/student_course.sql
+++ sql/student_course.sql
@@ -73,3 +73,10 @@ SELECT *
 FROM db_sc.student_course;
 
 INSERT INTO db_sc.student_course VALUES (1, 1, NULL);-- 不可以添加，因为有联合主键
+
+-- id = 1的学生选了哪些课？
+SELECT *
+FROM db_sc.student s INNER JOIN db_sc.course c INNER JOIN db_sc.student_course sc
+ON s.id = sc.courseId AND c.id = sc.courseId
+WHERE s.id = 1;
+
