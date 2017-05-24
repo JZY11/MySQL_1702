0fb844a2baeaaff49378514810785d1c419bafdc
Tony.Jaa <1291813139@qq.com>
2017-05-24 14:21:34 +0800

    updata

--- sql/day10.sql
+++ sql/day10.sql
@@ -1,4 +1,5 @@
 START TRANSACTION ;
+
 SELECT *
 FROM scott.emp;
 
@@ -6,3 +7,22 @@ TRUNCATE TABLE scott.emp; -- truncate是比较危险的操作，彻底删除，�
 
 DELETE FROM scott.emp;
 ROLLBACK ; -- rollback回滚   在开启一次事物之后delete掉表中的数据后是可以rollback回来的
+
+SHOW TABLES IN scott;
+
+START TRANSACTION ;
+
+UPDATE scott.emp
+    SET SAL = SAL + 1000
+WHERE ENAME = 'allen';
+
+UPDATE scott.emp
+    SET SAL = SAL - 2000
+WHERE ENAME = 'ward';
+
+COMMIT ; -- 事物的完成 （提交），事物有开启就要有完成
+
+ROLLBACK ;
+
+SELECT *
+FROM scott.emp;
--- sql/day9.sql
+++ sql/day9.sql
@@ -1,6 +1,7 @@
 SELECT *
 FROM scott.dept;
 
+
 CREATE VIEW scott.v_emp_10
   AS
   SELECT *
@@ -21,3 +22,10 @@ SHOW TABLE STATUS FROM db_sc;
 SHOW CREATE TABLE db_sc.student;
 
 SHOW CREATE VIEW scott.v_emp_10;-- 显示建视图语句
+
+SELECT *
+FROM scott.v_emp_10
+WHERE JOB = 'manager';
+
+SELECT *
+FROM scott.emp;
