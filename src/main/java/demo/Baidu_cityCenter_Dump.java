e76d52e268673485d357cde48f74326fbbfe36f0
Tony.Jaa <1291813139@qq.com>
2017-05-25 11:44:35 +0800

    updata

--- src/main/java/demo/Baidu_cityCenter_Dump.java
+++ src/main/java/demo/Baidu_cityCenter_Dump.java
@@ -19,7 +19,7 @@ public class Baidu_cityCenter_Dump {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/";
     private static final String USER = "root";
     private static final String PASSWORD = "system";
-    private static final String SQL = "INSERT INTO db_1712.baidumap VALUES (NULL,?, ?)";
+    private static final String SQL = "INSERT INTO db_1712.baidumap VALUES (NULL,?, ?,?)";
     //    ?号表示未定义参数
     private static int counter;
     private static PreparedStatement preparedStatement;
@@ -31,13 +31,14 @@ public class Baidu_cityCenter_Dump {
         connection.setAutoCommit(false); //*** commit 事务  提交
         preparedStatement = connection.prepareStatement(SQL);
 //        读取文件
-        try(BufferedReader reader = new BufferedReader(new FileReader("data/BaiduMap_cityCenter.txt"))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/main/java/demo/BaiduMap_cityCenter.txt"))) {
             String line;
             while ((line = reader.readLine()) != null ){//  逐行读取
-                String loc = line.split(".[n]:\\s ")[0];//  处理每行数据，分割成三个部分
-                String geo = line.split(".[g]:\\s")[0];//   \s+ 表示空格至少有一个可以有多个
+                String loc = line.split(",")[0];//  处理每行数据，分割成三个部分
+                String geo = line.split(",")[1];//   \s+ 表示空格至少有一个可以有多个
+                String geo1 = line.split(",")[2];//   \s+ 表示空格至少有一个可以有多个
 //                String geo = line.replace(min,"").replace(max,"").trim();
-                dump(loc,geo);
+                dump(loc,geo,geo1);
                 if(++counter % 10000 == 0){
                     System.out.println(counter);
                     preparedStatement.executeBatch(); // ***
@@ -51,12 +52,17 @@ public class Baidu_cityCenter_Dump {
         preparedStatement.close();
         connection.close();
         System.out.println("total time:" + (System.currentTimeMillis() - start) / 1000 + "seconds");
+        System.out.println();
+
+        //System.out.println({name: '海门', value: [121.15, 31.89, 90]});
     }
-    private static void dump(String loc,String geo) throws SQLException {
+    private static void dump(String loc, String geo, String geo1) throws SQLException {
         preparedStatement.setString(1,loc);
         preparedStatement.setString(2,geo);
+        preparedStatement.setString(3,geo1);
 //        preparedStatement.setString(3,geo);
 
+        System.out.println("{name: " + loc + ", " + " value: [" + geo + ", " + geo1 + "]}" + ",");
         preparedStatement.addBatch();//batch xxx.bat  批处理程序
     }
 }
--- src/main/java/demo/test.sql
+++ src/main/java/demo/test.sql
@@ -1,5 +1,5 @@
 
-LOAD DATA LOCAL INFILE 'C:\Users\Tony.Jaa\Desktop.BaiduMap_cityCenter.txt'
+LOAD DATA LOCAL INFILE 'C:\\Users\\Tony.Jaa\\IdeaProjects\\MySQL_1702\\src\\main\\java\\demo\\BaiduMap_cityCenter.txt'
 INTO TABLE db_1712.baidumap
-FIELDS TERMINATED BY ',' (loc, geo)
+FIELDS TERMINATED BY ',' (loc, geo,geo1)
 SET id = NULL;
