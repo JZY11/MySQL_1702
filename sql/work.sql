79c35193a377314001d6d087477566442fdedaca
Tony.Jaa <1291813139@qq.com>
2017-06-01 10:11:54 +0800

    updata

--- sql/work.sql
+++ sql/work.sql
@@ -18,6 +18,12 @@ CREATE TABLE db_work.log(
   useID INT COMMENT 'FK 用户ID'
 )COMMENT '工作日志表';
 
+ALTER TABLE db_work.log
+    ADD CONSTRAINT
+log_fk_userId
+FOREIGN KEY (useID)
+  REFERENCES db_work.user(id);
+
 INSERT INTO db_work.user(username,password,team)VALUE ('emp1','123','第一组');
 INSERT INTO db_work.user (username, password, team) VALUE ('emp2', '123', '第一组');
 INSERT INTO db_work.user (username, password, team) VALUE ('emp3', '123', '第二组');
@@ -47,7 +53,31 @@ WHERE useID = 1;
 
 -- 组长查看本组的日志
 SELECT u.username,l.time,l.content
-FROM db_work.user u INNER JOIN db_work.log l
+FROM db_work.user u LEFT OUTER JOIN db_work.log l -- 左外联结*** 显示左表的落选记录
 ON u.id = l.useID
 WHERE u.team = '第一组';
+
+
+SELECT u.username,l.time,l.content
+FROM db_work.user u LEFT OUTER JOIN db_work.log l -- 左外联结***
+    ON u.id = l.useID
+WHERE u.team = '第一组' AND u.username = 'emp4';
+
+
+SELECT
+  u.username,
+  l.time,
+  l.content
+FROM db_work.user u LEFT OUTER JOIN db_work.log l -- 左外联结***
+    ON u.id = l.useID
+WHERE u.team = '第一组' AND l.time = '2017-6-1';
+
+
+SELECT
+  u.username,
+  l.time,
+  l.content
+FROM db_work.user u LEFT OUTER JOIN db_work.log l -- 左外联结***
+    ON u.id = l.useID
+WHERE u.team = '第一组' AND l.time = '2017-5-31' AND u.username = 'emp2';
 
