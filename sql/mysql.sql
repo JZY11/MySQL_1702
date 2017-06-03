b7e9e6a10dbc371a4a44516f8fc2efe281c04e6e
Tony.Jaa <1291813139@qq.com>
2017-06-03 14:33:31 +0800

    updata

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -22,4 +22,21 @@ CREATE TABLE db_test.Score(
 ALTER TABLE db_test.Score
     ADD CONSTRAINT
 Score_fk_studentId
-PRIMARY KEY (studentId)
+FOREIGN KEY (studentId)
+  REFERENCES db_test.Student(id);
+
+INSERT INTO db_test.Student VALUE (NULL ,'Mark','M','1995-06-07','计算机系','人民路5号');
+INSERT INTO db_test.Student VALUE (NULL ,'Marry','F','1996-06-07','美术系','人民路6号');
+INSERT INTO db_test.Student VALUE (NULL ,'James','M','1994-06-07','机械工程系','向阳路5号');
+INSERT INTO db_test.Student VALUE (NULL ,'Lily','F','1995-06-07','计算机系','向阳路5号');
+INSERT INTO db_test.Student VALUE (NULL ,'Anni','F','1995-06-07','音乐系','向阳路5号');
+
+INSERT INTO db_test.Score VALUE (NULL ,2,'Java SE','大一');
+INSERT INTO db_test.Score VALUE (NULL ,1,'素描','大三');
+INSERT INTO db_test.Score VALUE (NULL ,3,'机械设计','大三');
+INSERT INTO db_test.Score VALUE (NULL ,5,'Java EE','大二');
+INSERT INTO db_test.Score VALUE (NULL ,4,'音乐鉴赏','大一');
+
+SELECT *
+FROM db_test.Student;
+
